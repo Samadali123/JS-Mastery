4b408c92bfdff4ae404c20a9d9cd8c88cbcac666
syed samad ali <samadali0125@gmail.com>
2025-02-16 15:00:06 +0530

    understand operators in js

---
+++ operators.js
@@ -0,0 +1,23 @@
+// Operators in js
+
+// Arithmetic Operators
+
+console.log(5 + 2);   // 7
+console.log(5 - 2);   // 3
+console.log(5 * 2);   // 10
+console.log(5 / 2);   // 2.5
+console.log(5 % 2);   // 1
+
+
+// Comparison Operators
+console.log(5 == "5");  // true  (loose equality)
+console.log(5 === "5"); // false (strict equality)
+console.log(5 != 3);    // true
+console.log(5 > 3);     // true
+
+
+// logical operators
+console.log(true && false); // false
+console.log(true || false); // true
+console.log(!true);         // false
+
