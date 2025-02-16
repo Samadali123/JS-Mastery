cdbb53a0f117c40608b4cedaa0761db493143055
syed samad ali <samadali0125@gmail.com>
2025-02-16 14:46:43 +0530

    understand logging in js

---
+++ console.js
@@ -0,0 +1,13 @@
+// logging in js console.log,consoe.warn,console.error, etc
+// alert and prompt these two are not a part of js this is a part of browser window object
+
+
+console.log("This is a normal log.");
+console.warn("This is a warning!");
+console.error("This is an error!");
+let username = "Samad";
+console.log(`my username is ${username}`)
+
+// alert("Hello! Welcome to JavaScript.");
+// let userName = prompt("Enter your name:");  // Takes user input
+// console.log("User entered:", userName);
